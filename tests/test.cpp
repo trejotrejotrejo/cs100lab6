b3a8b8a38b9e44a197f39aa9d45cd8d66fbc5fd9
rohanbehera03 <rohanbehera03@gmail.com>
2021-05-07 20:14:09 -0700

    Added passing tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -7,5 +7,32 @@ int main(int argc, char **argv) {
   return RUN_ALL_TESTS();
 }
 
+TEST(RectangleTest, DefaultArea) {
+	Rectangle* rect = new Rectangle();
+	EXPECT_EQ(rect->area(), 0);
+	delete rect;
+}
 
+TEST(RectangleTest, AreaPositive) {
+	Rectangle* rect = new Rectangle(3, 5);
+	EXPECT_EQ(rect->area(), 15);
+	delete rect;
+}
 
+TEST(RectangleTest, PerimeterPositive) {
+	Rectangle* rect = new Rectangle(3, 5);
+	EXPECT_EQ(rect->perimeter(), 16);
+	delete rect;
+}
+
+TEST(RectangleTest, DecimalArea) {
+	Rectangle* rect = new Rectangle(2, 3.2);
+	EXPECT_EQ(rect->area(), 6);
+	delete rect;
+}
+
+TEST(RectangleTest, DecimalPerimeter) {
+	Rectangle* rect = new Rectangle(2, 3.2);
+	EXPECT_EQ(rect->perimeter(), 10);
+	delete rect;
+}

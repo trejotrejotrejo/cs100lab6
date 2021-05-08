53ad1e30830978aeb45cf4aa5ab92be7741e3c78
rohanbehera03 <rohanbehera03@gmail.com>
2021-05-07 23:03:41 -0700

    Updated main.yml and test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -38,20 +38,4 @@ TEST(RectangleTest, DecimalPerimeter) {
 }
 
 
-TEST(RectangleTest, PerimeterPositive) {
-	Rectangle* rect = new Rectangle(3, 5);
-	EXPECT_EQ(rect->perimeter(), 16);
-	delete rect;
-}
 
-TEST(RectangleTest, DecimalArea) {
-	Rectangle* rect = new Rectangle(2, 3.2);
-	EXPECT_EQ(rect->area(), 6);
-	delete rect;
-}
-
-TEST(RectangleTest, DecimalPerimeter) {
-	Rectangle* rect = new Rectangle(2, 3.2);
-	EXPECT_EQ(rect->perimeter(), 10);
-	delete rect;
-}

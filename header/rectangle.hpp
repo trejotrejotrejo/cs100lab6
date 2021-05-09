dd9d4c649955da1bc006adc580a462ecabef625a
rohanbehera03 <65427904+rohanbehera03@users.noreply.github.com>
2021-05-09 11:21:31 -0700

    Update rectangle.hpp

--- header/rectangle.hpp
+++ header/rectangle.hpp
@@ -11,8 +11,8 @@ class Rectangle {
 
         void set_width(int w);
         void set_height(int h);
-        int get_width;
-        int get_height;
+        int get_width();
+        int get_height();
         int area();
         int perimeter();
 };
